9d96558f791669c1164a45967d187b60889b8e1f
p22rstewart <p22rstewart@fairfieldprep.org>
2021-11-03 09:57:48 -0400

    .

--- hw5-4.py
+++ hw5-4.py
@@ -25,7 +25,7 @@ print(str(54321))
 print("p" not in "Pineapple")
 
 # Question 9
-print("\"Livin\' On a Prayer\" is my friend\'s favorite song.")
+print("\"Livin' On a Prayer\" is my friend's favorite song.")
 
 # Question 10
-print(len("\"Livin\' On a Prayer\" is my friend\'s favorite song."))
+print(len("\"Livin' On a Prayer\" is my friend's favorite song."))
