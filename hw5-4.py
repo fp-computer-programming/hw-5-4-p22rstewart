686fc61062adb104b176a3deb37f9d45e698b8b6
Ryan Stewart <P22rstewart@fairfieldprep.org>
2021-11-03 00:39:29 -0400

    5-4

---
+++ hw5-4.py
@@ -0,0 +1,31 @@
+# Author RTS 11/3/21
+
+# Question 1
+print("python"[2])
+
+# Question 2
+print("Strings are simply a sequence of characters."[30])
+
+# Question 3
+print(len("excellent"))
+
+# Question 4
+print("apple" in "pineapple")
+
+# Question 5
+print("Peach" < "pearl")
+
+# Question 6
+print("x" * 100)
+
+# Question 7
+print(str(54321))
+
+# Question 8
+print("p" not in "Pineapple")
+
+# Question 9
+print("\"Livin\' On a Prayer\" is my friend\'s favorite song.")
+
+# Question 10
+print(len("\"Livin\' On a Prayer\" is my friend\'s favorite song."))
